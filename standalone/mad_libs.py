afac9981c7989d2db4e06b60ddf3716419c35c98
rishiT243 <cupertino+rtammineedi@thecoderschool.com>
2025-04-12 14:44:31 -0700

    Create mad_libs.py

---
+++ standalone/mad_libs.py
@@ -0,0 +1,25 @@
+# multi-line strings """ """
+# formatted strings, f""
+# normal strings, ""
+# blank = input(question)
+noun1 = input("Enter a noun: ")
+thing1 = input("Enter an object: ")
+thing2 = input("Enter an object: ")
+thing3 = input("enter any word: ")
+thing4 = input("enter an onamonapoeia: ")
+thing5 = input("enter a name: ")
+thing6 = input("enter an adjective: ")
+thing7 = input("enter an animal: ")
+thing8 = input("enter a food: ")
+madLib = f"""One sunny morning, a group of {noun1} adventurers set off on a journey into the wild, untamed Amazon jungle. 
+They packed {thing1}, {thing2}, and a giant {thing2}, just in case they encountered a {thing3} problem. As they walked 
+through the dense jungle, they suddenly heard a loud {thing4} "What was that?!" yelled {thing5}, looking around nervously. 
+Out of the trees, a massive {thing6} {thing7} appeared, covered in {thing1}. "This is it!" screamed {thing5}, "We’re 
+gonna get {thing3}!" But just then, a {thing1} {thing7} swooped down and stole their last bag of {thing8}! "No! Not the 
+{thing5}!" cried the group. "We’ll never survive the jungle without it!" They quickly followed the {thing2}creature into 
+the jungle, which led them to a {thing3} waterfall. Beneath the water, they saw something shiny, a {thing6} treasure chest! 
+"It’s the legendary {noun1} treasure!" shouted {thing5}. "Let’s open it!" But as they touched the chest, the ground began 
+to {thing4}, and a giant {thing3}{thing4}burst from the earth! "RUN!" yelled {thing5}. The group sprinted through the jungle, 
+dodging {noun1}and {thing3}, until they reached the safety of a {thing2} treehouse. Panting and out of breath, {thing5} wiped 
+their brow and sighed, "Well, at least we still have our {thing3}... and our {thing4} sense of adventure."""
+print(madLib)
